1ee6b07a21fc3870372b9cf91be5aeafce3b2cfd
ibeginnernoob <adheilgupta@gmail.com>
2026-08-02 20:03:39 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
+
+var vars = make(map[string]string)
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -32,25 +35,52 @@ func HandleConn(conn net.Conn) {
 
 		n, err := conn.Read(in)
 		if err != nil {
-			panic(fmt.Sprintf("Error reading from connection: %s\n", err.Error()))
+			fmt.Printf("Error reading from connection: %s\n", err.Error())
+			break
 		}
 
 		parts := RESPDecoder(n, in)
-		for _, v := range parts {
-			fmt.Printf("%q\n", v)
-		}
+		// for _, v := range parts {
+		// 	fmt.Printf("%q\n", v)
+		// }
 
 		switch parts[0] {
 		case "*1":
-			out = []byte("+PONG\r\n")
+			out = RESPEncoder("PONG", true)
 			if _, err := conn.Write(out); err != nil {
 				fmt.Printf("Error writing into connection: %s\n", err.Error())
 			}
 
 		case "*2":
 			if parts[1] == "$4" && strings.ToLower(parts[2]) == "echo" {
-				outs := fmt.Sprintf("%s\r\n%s\r\n", parts[3], parts[4])
-				out = []byte(outs)
+				out = RESPEncoder(parts[4], false)
+
+				if _, err := conn.Write(out); err != nil {
+					fmt.Printf("Error writing into connection: %s\n", err.Error())
+				}
+			} else if parts[1] == "$3" && strings.ToLower(parts[2]) == "get" {
+				key := parts[4]
+
+				val, ok := vars[key]
+				if !ok {
+					out = RESPEncoder("", false)
+				} else {
+					out = RESPEncoder(val, false)
+				}
+
+				if _, err := conn.Write(out); err != nil {
+					fmt.Printf("Error writing into connection: %s\n", err.Error())
+				}
+			}
+
+		case "*3":
+			if parts[1] == "$3" && strings.ToLower(parts[2]) == "set" {
+				key := parts[4]
+				val := parts[6]
+
+				vars[key] = val
+
+				out = RESPEncoder("OK", true)
 
 				if _, err := conn.Write(out); err != nil {
 					fmt.Printf("Error writing into connection: %s\n", err.Error())
@@ -61,7 +91,26 @@ func HandleConn(conn net.Conn) {
 }
 
 func RESPDecoder(n int, in []byte) []string {
-	// n-1 to remove the trailing \n
 	com := string(in[:n])
+
+	// print incoming bytes -> testing
+	// fmt.Printf("%q\n", in[:n])
+
 	return strings.Split(com, "\r\n")
+}
+
+// simple string -> +{string}\r\n
+// bulk string -> {string_len}\r\n{string}\r\n
+func RESPEncoder(res string, simple bool) []byte {
+	var s string
+	if simple {
+		s = fmt.Sprintf("+%s\r\n", res)
+	} else {
+		if len(res) == 0 {
+			s = "$-1\r\n"
+		} else {
+			s = fmt.Sprintf("$%s\r\n%s\r\n", strconv.Itoa(len(res)), res)
+		}
+	}
+	return []byte(s)
 }
